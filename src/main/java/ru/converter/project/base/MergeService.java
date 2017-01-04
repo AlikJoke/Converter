00b406e930c98d0ed02a546403dbdee8a51b9894
qqqqq qqqqq <qqqqq qqqqq>
2017-01-04 17:35:05 +0300

    Implements start() for Merge service

--- src/main/java/ru/converter/project/base/MergeService.java
+++ src/main/java/ru/converter/project/base/MergeService.java
@@ -1,9 +1,15 @@
 package ru.converter.project.base;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.google.common.net.MediaType;
 
 public class MergeService extends AbstractService {
 
@@ -12,10 +18,28 @@ public class MergeService extends AbstractService {
 		return System.getProperty("java.io.tmpdir") + "//merge";
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	File start(List<MultipartFile> files, String name) {
-		// TODO start() for MergeService
-		return null;
+		if (files == null || files.isEmpty() || !StringUtils.hasLength(name))
+			throw new RuntimeException("List of files and name of file should be not null or empty");
+		PDFMergerUtility ut = new PDFMergerUtility();
+		files.stream().filter(file -> file.getContentType().equalsIgnoreCase(MediaType.PDF.toString()))
+				.forEach(file -> {
+					try {
+						ut.addSource(new ConverterService().start(Arrays.asList(file), name));
+					} catch (IOException e) {
+						throw new RuntimeException("Error while merge documents: stage convert to pdf");
+					}
+				});
+		String filename = tempDirectory() + "//" + name;
+		ut.setDestinationFileName(filename);
+		try {
+			ut.mergeDocuments();
+			return new File(filename);
+		} catch (IOException e) {
+			throw new RuntimeException("Error while merge documents");
+		}
 	}
 
 }
